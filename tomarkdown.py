a2272390db4ebda31d2783ba38d46a1b38b813e9
limbail <limbail@hotmail.com>
2023-03-26 15:00:01 +0200

    Convert to md format

---
+++ tomarkdown.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_excel('Vocabulary.xlsx')
+df = df.apply(lambda x: x.str.replace('\n', '<br>') if x.dtype == 'object' else x)
+md_table = df.to_markdown(index=False)
+#print(md_table)
+t_content=df.to_markdown()
+f = open("vocabulary.md", "a")
+f.write(t_content)
+f.close()
+
